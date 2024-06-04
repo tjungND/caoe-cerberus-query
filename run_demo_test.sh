4fc10e54bf7bddeb4db0ecaa953260afcef81559
Nirajan Koirala <31283818+n7koirala@users.noreply.github.com>
2024-06-04 10:43:58 -0400

    Update run_demo_test.sh

--- run_demo_test.sh
+++ run_demo_test.sh
@@ -13,5 +13,5 @@
 # Create or overwrite the result.txt file
 > ckks_16parties_7bits.txt
 
-{ time NUM_KEY_SHARES=$NUM_KEY_SHARES INTERSECTION=$INTERSECTION SENDER_BITS=$SENDER_BITS DEPTH=$DEPTH NUM_PARTIES=$NUM_PARTIES TYPE=$TYPE ./demo_aug31.sh; } 2>&1 | tee -a ckks_16parties_7bits.txt
+{ time NUM_KEY_SHARES=$NUM_KEY_SHARES INTERSECTION=$INTERSECTION SENDER_BITS=$SENDER_BITS DEPTH=$DEPTH NUM_PARTIES=$NUM_PARTIES TYPE=$TYPE ./demo_test.sh; } 2>&1 | tee -a ckks_16parties_7bits.txt
 
